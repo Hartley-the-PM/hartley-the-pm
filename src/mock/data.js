b5c63a086bb86876e75089d8e5c4ef0d1352a052
sh-to-dev <85166590+sh-to-dev@users.noreply.github.com>
2021-08-22 12:09:18 -0300

    Typos and Meta Data
    Fixed typos in the copy. Updated the meta data to update the site branding to Hartley the PM.

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Hartley the PM',
+  lang: 'en',
+  description: 'Portfolio',
 };
 
 // HERO DATA
@@ -40,7 +40,7 @@ export const projectsData = [
     info2:
       'To achieve this, Hartley modelled potential user growth to inform the profitability of the initiative, the negotiation process, and supply chain inputs; he then collaberated with legal to create the agreement. He also launched the initiative via a robust project roadmap and onboarded users by facilitating product training.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
@@ -48,18 +48,18 @@ export const projectsData = [
     title: 'Medical Device E-Catalog',
     info: 'Hartley mapped out and orchestrated the development of an E-catalog in the MDS-Canada business unit, leading to improved user experience. This new tool provided customers with the ability to learn more about products in real time without speaking to the customer support team at BD-Canada.',
     info2:
-      'Hartley gathered all product related information and developed content criteria based on internal feedback, along with strategically choosing product images. He then worked with the global development and digit marketing teams to construct and design the layout of the platform.',
+      'Hartley gathered all product related information and developed content criteria based on internal feedback, along with strategically choosing product images. He then worked with the global development and digital marketing teams to construct and design the layout of the platform.',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'undraw_medicine.png',
-    title: 'Something Awesome is in the Works',
-    info: 'Hartley is diligently working on more exciting independent projects, product management articles, and case studies. Stay tuned to learn more and join the community!',
+    title: 'Something Awesome is in the Works!',
+    info: 'Hartley is diligently working on more exciting projects, product management articles, and case studies. Stay tuned to learn more and join the community!',
     info2: '',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Hartley the PM'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Hartley the PM'} />
       </Helmet>
       <App />
     </>
